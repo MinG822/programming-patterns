2ac3a1fe5754339c0415798d4745d1d8e3ab0802
MinG822 <lucida822@naver.com>
2021-05-06 00:48:11 +0900

    fix 04 proceduresWithGlobalVars with maps & object

--- 04-cookbook/proceduresWithGlobalVars.js
+++ 04-cookbook/proceduresWithGlobalVars.js
@@ -3,8 +3,9 @@
 const fs = require('fs')
 
 let data = null
-const words = []
-const wordFreqs = []
+let words = []
+const wordFreqs = {}
+let sortedWordFreqs = []
 
 const readFile = (pathToFile) => {
     data = fs.readFileSync(pathToFile, "utf-8")
@@ -13,58 +14,53 @@ const readFile = (pathToFile) => {
 const filterCharsAndNorm = () => {
     const alnumRegExp = new RegExp(/^[a-z0-9]+$/i)
     const isAlNum = (word) => word.replace(alnumRegExp, "").length === 0
+    let word = ''
     for (let i = 0; i < data.length ; i++) {
-        console.log(data[i])
         if (!isAlNum(data[i])) {
-            data[i] = ' '
+            if (word.length) {
+                words.push(word)
+                word = ''
+            }
         } else {
-            data[i] = data[i].toLowerCase()
+            word += data[i].toLowerCase()
         }
     }
-}
-
-const scan = () => {
-    words.push(...data.split())
 }
 
 const removeStopWords = () => {
     let stopWords = fs.readFileSync('../stop_words.txt','utf-8').split(',')
     let lowerAlpahs = Array(26).fill(1).map((_, i) => String.fromCharCode( 97 + i ))
     stopWords = stopWords.concat(...lowerAlpahs)
+
     const indexes = []
     for (let i = 0; i < words.length; i++) {
         if (stopWords.includes(words[i])) {
             indexes.push(i)
         }
     }
-    for (let i = 0; i < words.length; i++) {
-        words.pop()
-    }
+    words = words.filter(w => !stopWords.includes(w))
 }
 
 const frequencies = () => {
     for (const word of words) {
-        const keys = wordFreqs.map(wordAndFreq => wordAndFreq[1])
-        if (keys.includes(word)) {
-            wordFreqs[keys.indexOf(word)] += 1
+        if (word in wordFreqs) {
+            wordFreqs[word] += 1
         } else {
-            wordFreqs.push([word, 1]) 
+            wordFreqs[word] = 1 
         }
     }
 }
 
 const sortFreqs = () => {
-    wordFreqs.sort((a, b) => b[1] - a[1])
+    sortedWordFreqs = Object.keys(wordFreqs).sort((a, b) => wordFreqs[b] - wordFreqs[a])
 }
 
 readFile(process.argv[2])
 filterCharsAndNorm()
-console.log(data.length)
-scan()
 removeStopWords()
 frequencies()
 sortFreqs()
 
-for(const tf of wordFreqs.slice(0,25)) {
-    console.log(tf[0], '  -  ', tf[1])
+for(const a of sortedWordFreqs.slice(0,25)) {
+    console.log(a, '  -  ', wordFreqs[a])
 }
